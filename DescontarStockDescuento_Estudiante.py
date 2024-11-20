c8e63ce74a2fd442c1dbc879d5b71bb205c18c72
edson123450 <142638227+edson123450@users.noreply.github.com>
2024-11-19 22:15:15 -0500

    Update DescontarStockDescuento_Estudiante.py

--- DescontarStockDescuento_Estudiante.py
+++ DescontarStockDescuento_Estudiante.py
@@ -1,10 +1,31 @@
 import boto3
 from boto3.dynamodb.conditions import Key
+import json
 
 def lambda_handler(event,context):
     partition_key=event['body']['tenant_id#c_estudiante']
     sort_key=event['body']['c_descuento']
-    datos_descuento=['body']['datos_descuento']
+    datos_descuento=event['body']['datos_descuento']
+
+    # Inicio - Proteger el Lambda
+    token = event['headers']['Authorization']
+    lambda_client = boto3.client('lambda')
+    payload_string = '{ "token": "' + token + '" }'
+    invoke_response = lambda_client.invoke(
+        FunctionName="ValidarTokenEstudiante",
+        InvocationType='RequestResponse',
+        Payload=payload_string
+    )
+    response = json.loads(invoke_response['Payload'].read())
+    print(response)
+    if response['statusCode'] == 403:
+        return {
+            'statusCode': 403,
+            'status': 'Forbidden - Acceso no autorizado'
+        }
+    # Fin - Proteger el Lambda
+
+
 
     dynamodb=boto3.resource('dynamodb')
     table=dynamodb.Table('tabla_descuentos')
@@ -14,7 +35,7 @@ def lambda_handler(event,context):
             'c_descuento':sort_key
         },
         UpdateExpression="set datos_descuento=:datos_descuento",
-        ExpressionAtributeValues={
+        ExpressionAttributeValues={
             ':datos_descuento': datos_descuento
         },
         ReturnValues="UPDATED_NEW"
